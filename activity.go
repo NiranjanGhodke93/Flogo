c0b6320ea035a09345ca64032978966a7109c7c0
prajaktapandit <38352527+prajaktapandit@users.noreply.github.com>
2018-04-18 14:20:40 +0530

    Add files via upload

---
+++ activity.go
@@ -0,0 +1,54 @@
+package TestConnection
+
+import (
+    
+    "database/sql"
+    _ "github.com/mattn/go-oci8"
+	"github.com/TIBCOSoftware/flogo-lib/logger"	
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+
+	
+)
+
+// log is the default package logger which we'll use to log
+	var log = logger.GetLogger("activity-TestConnection")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	
+	sqlurl := context.GetInput("SqlURL").(string)
+	
+	
+    db, err := sql.Open("oci8",sqlurl)
+    if err != nil {
+         log.Debugf("Connection Refused");
+        return
+    }
+    defer db.Close()
+    
+    
+    if err = db.Ping(); err != nil {
+     
+	  log.Debugf("Error connecting to the database: [%s]",err);
+        return
+    }
+	
+	context.SetOutput("output","")
+	return true, nil
+}
